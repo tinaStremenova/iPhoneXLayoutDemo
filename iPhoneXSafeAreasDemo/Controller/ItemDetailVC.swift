0abd650bfd2f225e9bc2d805e2c478eba1c9377e
Tina Stremenova <martinastremenova@gmail.com>
2017-10-07 19:42:52 +0200

    [ItemDetail] Adding function for managing scrollview insets on iOS11.

--- iPhoneXSafeAreasDemo/Controller/ItemDetailVC.swift
+++ iPhoneXSafeAreasDemo/Controller/ItemDetailVC.swift
@@ -16,6 +16,7 @@ class ItemDetailVC: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var longDescriptionLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +25,52 @@ class ItemDetailVC: UIViewController {
         self.nameLabel.text = item.itemName
         self.descriptionLabel.text = item.itemDescription
         
-        
         if #available(iOS 11.0, *) {
             self.navigationItem.largeTitleDisplayMode = .never
         }
+        
+        // test insets behaviour
+        self.setInsets()
+    }
+    
+    private func setInsets() {
+        
+        // content inset to inset content for developpers
+        self.scrollView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
+        
+        if #available(iOS 11.0, *) {
+            
+            // When constraining scrollview to superview, you can controll whether it takes into account safe areas
+            // Automatic setting is default and takes value set in viewcontroller
+            // If you constrain scrollview to safe area, you don't have to bother with this
+            self.scrollView.contentInsetAdjustmentBehavior = .never
+            
+            // When false, scrollview goes under the navigation bar if contrained to superview instead of safe area
+            // Scrollview property contentInsetAdjustmentBehavior overrides this behaviour if not set to .automatic
+            self.automaticallyAdjustsScrollViewInsets = false
+            
+            // Constrains label to scrollview frame to fixed position
+            // The layout guide based on the untransformed frame rectangle of the scroll view - so it does not take account of the insets or safe areas
+            let frameLayoutGuide = self.scrollView.frameLayoutGuide
+            let label = self._makeEmployeeOfTheMonthLabel()
+            
+            self.scrollView.addSubview(label)
+            
+            // Use scrollView.adjustedContentInset so label will be constrained as all scrollview content
+            label.topAnchor.constraint(equalTo: frameLayoutGuide.topAnchor, constant: self.scrollView.adjustedContentInset.top).isActive = true
+            label.trailingAnchor.constraint(equalTo: frameLayoutGuide.trailingAnchor, constant: self.scrollView.adjustedContentInset.right).isActive = true
+        }
+    }
+    
+    private func _makeEmployeeOfTheMonthLabel() -> UILabel {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Employee of the month!"
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.backgroundColor = UIColor.STRV.red
+        label.textColor = .white
+        return label
     }
 }
